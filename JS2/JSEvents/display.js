eb021444bc988f1181c1522bf7aeb7ec5dd60d31
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-06-30 17:15:08 +0530

    okie

---
+++ JS2/JSEvents/display.js
@@ -0,0 +1,8 @@
+let display = () => {
+    //console.log("Hello,Good Evening")
+    alert("Hello,GE")
+}
+
+let getData = () => {
+    console.log("GM")
+}
---
+++ JS2/eventAssignment/one.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    document.getElementById('abc').style.background = 'red'
+    console.log(document.getElementById('abc'))
+}
+
+function clickColor() {
+    document.getElementById('xyz').style.background = "blue"
+}
